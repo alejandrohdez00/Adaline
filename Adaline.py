cf7be2e1a17a98d78d197d59abdcd626866a31f1
100405981 <100405981@alumnos.uc3m.es>
2021-09-21 18:22:16 +0200

    Comments

--- Adaline.py
+++ Adaline.py
@@ -1,4 +1,5 @@
 import numpy as np
+import panda as pd
 import random
 
 class Adaline:
@@ -11,4 +12,26 @@ class Adaline:
         for x in range(numero_entradas):
             self.pesos[x] = random.random()
         self.salida = 0
+
+    def ajustarPesos(self, entradas, salida_esperada):
+        #Calculamos diferencia entre salida esperada y salida
+        diferencia = salida_esperada - self.salida
+
+        #Actualizamos umbral
+        incremento_u = self.learning_rate*diferencia
+        self.umbral += incremento_u
+
+        #Actualizamos pesos
+        for i in range(len(self.pesos)):
+            incremento_p = self.learning_rate*diferencia*entradas[i]
+            self.pesos[i] += incremento_p
+
+    def calculoSalida(self, entradas):
+        #Calculamos el sumatorio de las entradas por su peso asignado
+        sumatorio = 0
+
+
+
+
+
 
